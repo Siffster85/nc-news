dae5b273b75b2c84f13afab1210dbf74bade67c0
Siffster85 <ylsaxon@googlemail.com>
2024-03-14 10:27:59 +0000

    Added error catcher

--- src/Components/CommentsListCard.jsx
+++ src/Components/CommentsListCard.jsx
@@ -1,27 +1,42 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert';
 import dateFormat from 'dateformat';
 import { useContext, useState } from 'react'
 import ActiveUserContext from '../Context/ActiveUser'
 import { deleteComment } from '../api';
+import Container from 'react-bootstrap/esm/Container';
 
 
 const CommentListCard = (props) => {
   const { activeUser } = useContext(ActiveUserContext)
-  const [ deletedComment, setDeletedCommet ] = useState()
+  const [error, setError] = useState('')
+	const [showError, setShowError] = useState(false) 
 
   function handleDelete() {
     const commentID = props.comment.comment_id
 		deleteComment(commentID)
+      .then(() =>{
+      })
 			.then(() => {
 				props.setComments((currentComments)=> {
 					return currentComments.filter((comment) => comment.comment_id !== commentID)
 				})
+			})			
+      .catch((err) => {
+				setError(err.msg)
+				setShowError(true)
 			})
   }
 
   if (props.comment.author === activeUser.username) {
        return (
+       <Container>
+        <ShowMessage
+          showError={showError}
+          setShowError={setShowError}
+          error={error}
+        />
         <Card className="my-1 p-0 text-start">
             <Card.Subtitle className="mt-1 ms-1 text-muted">Posted At: {dateFormat(props.comment.created_at, "mmmm dS, yyyy, HH:MM")}</Card.Subtitle>
           <Card.Body className="ms-1">
@@ -34,6 +49,7 @@ const CommentListCard = (props) => {
             <Button className="ms-2" variant="danger" onClick={handleDelete}>Delete</Button></Card.Text>
           </Card.Body>
         </Card>
+        </Container>
       );}
       else {
         return (
@@ -51,4 +67,19 @@ const CommentListCard = (props) => {
       );
       }
     }
+
+  function ShowMessage({ showError, setShowError, error}) {
+      if (showError) {
+        return (
+          <Alert
+            variant="danger"
+            onClose={() => setShowError(false)}
+            dismissible
+          >
+          <p>{error}</p>
+          </Alert>
+          )
+        }
+    }
+
 export default CommentListCard
