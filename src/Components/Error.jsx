154461be8082df7ca67ec19d1b6e79f9db6f93d5
Siffster85 <ylsaxon@googlemail.com>
2024-03-15 11:36:41 +0000

    Deployed and added the non-existant path error I missed

---
+++ src/Components/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h2>This Page Doesn't Exist</h2>
+            <h3>Please return to the <a href="/">homepage</a> and try again</h3>
+        </div>
+    );
+};
+
+export default Error;
